4119bfda850a4ceb39df345f2ca3af89dfdbbcd7
Jacky <Jj0932175456@gmail.com>
2022-02-11 07:57:21 +0800

    a

--- routes/interactive_other.js
+++ routes/interactive_other.js
@@ -2,96 +2,85 @@ const Route = require("express").Router();
 const user = require("../models/userModel");
 // const Auth = require("../routes/Auth");
 
-
-
-
-
 // make friend
-Route.post("/makeFrinend",(req,res)=>{
-    let { target_id,my_id } = req.body
-    user.findUser(my_id,(me) =>{
-        let isExist = me.friend.findIndex((res)=>{
-            return res.equals(target_id)
-        })
-        if(isExist == -1){
-            me.makeFriendTool(target_id)
-            user.findUser(target_id,(friend)=>{
-                friend.makeFriendTool(my_id)
-            })
-        }else{
-            console.log("此好友已經存在了")
-        }
-        console.log(me)
-        console.log(isExist)
-        console.log(req.body)
-    })
-})
-
-
-
+Route.post("/makeFrinend", (req, res) => {
+	let { target_id, my_id } = req.body;
+	user.findUser(my_id, (me) => {
+		let isExist = me.friend.findIndex((res) => {
+			return res.equals(target_id);
+		});
+		if (isExist == -1) {
+			me.makeFriendTool(target_id);
+			user.findUser(target_id, (friend) => {
+				friend.makeFriendTool(my_id);
+			});
+		} else {
+			console.log("此好友已經存在了");
+		}
+		console.log(me);
+		console.log(isExist);
+		console.log(req.body);
+	});
+});
 
 // delete friend
-Route.post("/deleteFriend",(req,res)=>{
-    let { target_id,my_id } = req.body
-    user.findUser(my_id,(me)=>{
-        let friendIndex = me.friend.findIndex((res)=>{
-            return res.equals(target_id) 
-        })
-        me.friend.splice(friendIndex,1)
-        me.save()
-        user.findUser(target_id,(Friend)=>{
-            let myIndex = Friend.friend.findIndex((res)=>{
-                return res.equals(my_id)
-            })
-            Friend.friend.splice(myIndex,1)
-            Friend.save()
-            console.log("delete success")
-        })
-    })
-})
+Route.post("/deleteFriend", (req, res) => {
+	let { target_id, my_id } = req.body;
+	user.findUser(my_id, (me) => {
+		let friendIndex = me.friend.findIndex((res) => {
+			return res.equals(target_id);
+		});
+		me.friend.splice(friendIndex, 1);
+		me.save();
+		user.findUser(target_id, (Friend) => {
+			let myIndex = Friend.friend.findIndex((res) => {
+				return res.equals(my_id);
+			});
+			Friend.friend.splice(myIndex, 1);
+			Friend.save();
+			console.log("delete success");
+		});
+	});
+});
 
 // inquire friend info
-Route.post("/find_all_friends",(req,res)=>{
-    user.findOne({"_id" : req.body._id},(err,User)=>{
-        if(err){
-            console.log(err)
-        }else{
-            console.log(User)
-            if(User.friend.length == 0){
-                res.send("no friend")
-                console.log("no friend")
-            }else{
-                let sortingArray = []
-                sortingArray.push(User)
-                sortingArray.sort((a,b)=>{
-                    return b.Month_energy - a.Month_energy
-                })
-                res.json(sortingArray)
-                console.log(User)
-                console.log("my friend")
-            }
-        }
-    }).populate("friend")
-    
-})
+Route.post("/find_all_friends", (req, res) => {
+	user
+		.findOne({ _id: req.body._id }, (err, User) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(User);
+				if (User.friend.length == 0) {
+					res.send("no friend");
+					console.log("no friend");
+				} else {
+					let sortingArray = [];
+					sortingArray.push(User);
+					sortingArray.sort((a, b) => {
+						return b.Month_energy - a.Month_energy;
+					});
+					res.json(sortingArray);
+					console.log(User);
+					console.log("my friend");
+				}
+			}
+		})
+		.populate("friend");
+});
 
+Route.get("/find_all_user", (req, res) => {
+	user.find({}, (err, User) => {
+		if (err) {
+			console.log(err);
+		} else {
+			User.sort((a, b) => {
+				return b.Month_energy - a.Month_energy;
+			});
+			console.log(User);
+			res.json(User);
+		}
+	});
+});
 
-
-Route.get('/find_all_user',(req,res)=>{
-    user.find({},(err,User)=>{
-        if(err){
-
-            console.log(err)
-        }else{
-            User.sort((a,b)=>{
-                return b.Month_energy - a.Month_energy
-            })
-            console.log(User)
-            res.json(User)
-        }
-    })
-})
-
-
-
-module.exports = Route
+module.exports = Route;
--- routes/store_data.js
+++ routes/store_data.js
@@ -1,160 +1,165 @@
 const Route = require("express").Router();
 const study = require("../models/studyModel");
-const user = require("../models/userModel")
+const user = require("../models/userModel");
 const moment = require("moment");
-const calMonth = require("../calcauletMonthEnergy")
 
 // func
-    // 計算將秒數轉為 XX:XX:XX 格式
+// 計算將秒數轉為 XX:XX:XX 格式
 
-function transSecondToFormat(sec){
+function transSecondToFormat(sec) {
+	let studyTimeSeconds = "";
+	let studyTimeMinutes = "";
+	let studyTimeHours = "";
 
-    let studyTimeSeconds = ""
-    let studyTimeMinutes = ""
-    let studyTimeHours = ""
+	if (sec > 60) {
+		//如果秒數大於60，將秒數轉換成整數
 
-    if( sec > 60 ) {//如果秒數大於60，將秒數轉換成整數
+		//獲取分鐘，除以60取整數，得到整數分鐘
+		studyTimeMinutes = parseInt(sec / 60);
+		//獲取秒數，秒數取佘，得到整數秒數
+		studyTimeSeconds = parseInt(sec % 60);
+		//如果分鐘大於60，將分鐘轉換成小時
+		if (studyTimeMinutes > 60) {
+			//獲取小時，獲取分鐘除以60，得到整數小時
+			studyTimeHours = parseInt(studyTimeMinutes / 60);
+			//獲取小時後取佘的分，獲取分鐘除以60取佘的分
+			studyTimeMinutes = parseInt(studyTimeMinutes % 60);
+		}
+	} else {
+		studyTimeSeconds = sec;
+	}
 
-        //獲取分鐘，除以60取整數，得到整數分鐘
-        studyTimeMinutes = parseInt(sec / 60);
-        //獲取秒數，秒數取佘，得到整數秒數
-        studyTimeSeconds = parseInt(sec % 60);
-        //如果分鐘大於60，將分鐘轉換成小時
-        if(studyTimeMinutes > 60) {
-            //獲取小時，獲取分鐘除以60，得到整數小時
-            studyTimeHours = parseInt(studyTimeMinutes / 60);
-            //獲取小時後取佘的分，獲取分鐘除以60取佘的分
-            studyTimeMinutes = parseInt(studyTimeMinutes % 60);
-        }
-    }else{
-        studyTimeSeconds = sec
-    }
+	// let studyTime = moment.utc(single_study_time*1000).format('HH:mm:ss');
+	// 將秒數轉換成 HH:mm:ss
 
-    // let studyTime = moment.utc(single_study_time*1000).format('HH:mm:ss');
-    // 將秒數轉換成 HH:mm:ss
+	let endTime = moment()
+		.utc(sec * 1000)
+		.format("YYYY/MM/DD HH:mm:ss");
+	let startTime = moment()
+		.subtract({
+			hours: studyTimeHours,
+			minutes: studyTimeMinutes,
+			seconds: studyTimeSeconds,
+		})
+		.utc(sec * 1000)
+		.format("YYYY/MM/DD HH:mm:ss");
+	let energy = sec / 60;
 
-    let endTime =  moment().utc(sec*1000).format('YYYY/MM/DD HH:mm:ss')
-    let startTime = moment().subtract({
-        "hours": studyTimeHours ,
-        "minutes": studyTimeMinutes,
-        "seconds": studyTimeSeconds
-    }).utc(sec*1000).format('YYYY/MM/DD HH:mm:ss')
-    let energy = sec / 60
-
-    return {
-        endTime,
-        startTime,
-        energy
-
-    }
+	return {
+		endTime,
+		startTime,
+		energy,
+	};
 }
 
-
-
 // api
-Route.post("/add_new_data",(req,res)=>{
-        let {  single_study_time } = req.body
+Route.post("/add_new_data", (req, res) => {
+	let { single_study_time } = req.body;
 
-        // 取得 { endTime,startTime,energy }
-        let timeData = transSecondToFormat(single_study_time)
-        
-        // 取得當月月份
-        let getThisMonth = moment().get('month') + 1
-        // 取得當年年份
-        let getThisyear = moment().get('year')
-        
-        //取得startTime的月份
-        let getStartTimeMonth = timeData.startTime.split("/",2)[1]
-        //取得startTime的年份
-        let getStartTimeyear = timeData.startTime.split("/",2)[0]
+	// 取得 { endTime,startTime,energy }
+	let timeData = transSecondToFormat(single_study_time);
 
+	// 取得當月月份
+	let getThisMonth = moment().get("month") + 1;
+	// 取得當年年份
+	let getThisyear = moment().get("year");
 
+	//取得startTime的月份
+	let getStartTimeMonth = timeData.startTime.split("/", 2)[1];
+	//取得startTime的年份
+	let getStartTimeyear = timeData.startTime.split("/", 2)[0];
 
-        // 是否與現在月份相同若有則只需加入一筆資料，若無(跨月問題)則要新增兩筆(當月、上月)，若無
-        
-        if(getThisMonth == getStartTimeMonth && getThisyear == getStartTimeyear){
-            // 相同月份及年份的情況下
-            study.create({
-                "topic": req.body.todo,
-                "start_study_time": timeData.startTime,
-                "duration" : single_study_time,
-                "end_study_time": timeData.endTime,
-                "energy": timeData.energy,
-                "user": req.body.userId
-            },(err,data)=>{
-                if(err){
-                    console.log(err)
-                }else{
-                        user.findUser(req.body.userId,(User)=>{
-                        User.all_energy += Number( single_study_time)/60
-                        User.Month_energy += Number( single_study_time)/60
-                        User.save()
-                    })
-                }
-            })
-            console.log("相同月份")
-        }else{
-            // 不同月份的情況下
-                // 年份相同
-                    // if( getThisyear == getStartTimeMonth){
-                    //     studt.create({
-                    //         "topic": req.body.todo,
-                    //         "start_study_time": timeData.startTime.format('YYYY/MM/DD HH:mm:ss'),
-                    //         "duration" : single_study_time,
-                    //         "end_study_time": ``,
-                    //         "energy": energy,
-                    //         "user": req.body.userId
-                    //     },(err,data)=>{
-                    //         if(err){
-                    //             console.log(err)
-                    //         }else{
-                                
-                    //         }
-                    //     })
-                // 不同年份
-                console.log(" 不同月份")
-            }
-        
-        
-    })
+	// 是否與現在月份相同若有則只需加入一筆資料，若無(跨月問題)則要新增兩筆(當月、上月)，若無
 
-// 查找相同標籤資料
+	if (getThisMonth == getStartTimeMonth && getThisyear == getStartTimeyear) {
+		// 相同月份及年份的情況下
+		study.create(
+			{
+				topic: req.body.todo,
+				start_study_time: timeData.startTime,
+				duration: single_study_time,
+				end_study_time: timeData.endTime,
+				energy: timeData.energy,
+				user: req.body.userId,
+			},
+			(err, data) => {
+				if (err) {
+					console.log(err);
+				} else {
+					user.findUser(req.body.userId, (User) => {
+						User.all_energy += Number(single_study_time) / 60;
+						User.Month_energy += Number(single_study_time) / 60;
+						User.save();
+					});
+				}
+			}
+		);
+		console.log("相同月份");
+	} else {
+		// 不同月份的情況下
+		// 年份相同
+		// if( getThisyear == getStartTimeMonth){
+		//     studt.create({
+		//         "topic": req.body.todo,
+		//         "start_study_time": timeData.startTime.format('YYYY/MM/DD HH:mm:ss'),
+		//         "duration" : single_study_time,
+		//         "end_study_time": ``,
+		//         "energy": energy,
+		//         "user": req.body.userId
+		//     },(err,data)=>{
+		//         if(err){
+		//             console.log(err)
+		//         }else{
 
+		//         }
+		//     })
+		// 不同年份
+		console.log(" 不同月份");
+	}
+});
+
+// 回傳當前使用者讀書資料(以物件形式作回傳)
+Route.get("/getUserStudyInfo", (req, res) => {
+	study.find({ user: "61bec7e175cd9b0527cf186c" }, (err, AllInfo) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.json(AllInfo);
+		}
+	});
+});
 
 // 取得能量經驗值(總)
 
-Route.post("/getEnergy",(req,res)=>{
-    let { _id } = req.body
-    user.findUser(_id,(User)=>{
-        let sum = 0
-        User.all_study_info.forEach ((item)=>{
-            sum += Number(item.energy)
-        })
-    res.send(String(sum))
-        
-    })
-    // console.log(_id)
-})
+Route.post("/getEnergy", (req, res) => {
+	let { _id } = req.body;
+	user.findUser(_id, (User) => {
+		let sum = 0;
+		User.all_study_info.forEach((item) => {
+			sum += Number(item.energy);
+		});
+		res.send(String(sum));
+	});
+	// console.log(_id)
+});
 
 //取得能量經驗值(當月)
 
-Route.post("/getMonthEnergy",(req,res)=>{
-    calMonth(req.body)
-})
+Route.post("/getMonthEnergy", (req, res) => {
+	calMonth(req.body);
+});
 
 // 讀書時使用
 
-
 // 刪除資料表
-// Route.get("/delete",(req,res)=>{
-//     study.remove({},(err,data)=>{
-//         if(err){
-//             console.log(err)
-//         }else{
-//             console.log(data)
-//         }
-//     })
-// })
+// Route.get("/delete", (req, res) => {
+// 	study.remove({}, (err, data) => {
+// 		if (err) {
+// 			console.log(err);
+// 		} else {
+// 			console.log(data);
+// 		}
+// 	});
+// });
 
-
-module.exports = Route
+module.exports = Route;
